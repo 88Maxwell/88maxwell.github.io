20f147b75a355e9f7f5ecc6ebd3321a0a84024f9
88Maxwell <88Maxwell@users.noreply.github.com>
2026-09-23 00:49:51 +0000

    deploy: 7cb736020f7149119ce554fba4014790507ef46f

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,4 +1,22 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"zRjr":[function(require,module,exports) {
-var e="pwabuilder-offline";importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"),self.addEventListener("message",function(e){e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()}),workbox.routing.registerRoute(new RegExp("/*"),new workbox.strategies.StaleWhileRevalidate({cacheName:e}));
-},{}]},{},["zRjr"], null)
-//# sourceMappingURL=pwabuilder-sw.js.map
+// This is the "Offline copy of assets" service worker
+
+// The cache name suffix is replaced with a timestamp on every build (see vite.config.js)
+const CACHE = "pwabuilder-offline-1790124591316";
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js");
+
+self.skipWaiting();
+workbox.core.clientsClaim();
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))),
+    );
+});
+
+workbox.routing.registerRoute(
+    new RegExp("/*"),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE,
+    }),
+);
